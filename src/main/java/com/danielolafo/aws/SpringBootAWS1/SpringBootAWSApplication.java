490b238816f09df0df78b64695a324ffcb71e643
danielolafo <danielolafo@gmail.com>
2026-09-01 10:32:09 -0500

    First commit

---
+++ src/main/java/com/danielolafo/aws/SpringBootAWS1/SpringBootAWSApplication.java
@@ -0,0 +1,14 @@
+package com.danielolafo.aws.SpringBootAWS1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SpringBootAWSApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAWSApplication.class, args);
+	}
+}
---
+++ src/main/java/com/danielolafo/aws/SpringBootAWS1/package-info.java
@@ -0,0 +1,1 @@
+package com.danielolafo.aws.SpringBootAWS1;
